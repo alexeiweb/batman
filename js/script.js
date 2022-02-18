3953ff9492f7c25acf65729e99b9485f42bf5a05
Alexei Web <alexeiiweb@gmail.com>
2022-02-18 14:16:48 +0300

    day 4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,38 @@
-const sliderThumbs = new Swiper('.slider-thumbs', {
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const navigationClose = document.querySelector('.navigation__close');
+
+burger.addEventListener('click', () => {
+  navigation.classList.add('navigation_active');
+});
+
+navigationClose.addEventListener('click', () => {
+  navigation.classList.remove('navigation_active');
+});
+
+try {
+  const mute = document.querySelector('.mute__checkbox');
+  const audio = new Audio('audio/waterTower.mp3');
+
+  const checkMute = () => {
+    if (mute.checked) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  };
+
+  if (mute) {
+    setTimeout(checkMute, 2000)
+  }
+
+  mute.addEventListener('change', checkMute);
+} catch {
+
+}
+  
+try {
+  const sliderThumbs = new Swiper('.slider-thumbs', {
   loop: true,
   spaceBetween: 20,
   slidesPerView: 3,
@@ -6,15 +40,33 @@ const sliderThumbs = new Swiper('.slider-thumbs', {
   loopedSlides: 4,
 });
 
-sliderThumbs.on('click', (swiper) => {
-  swiper.slideTo(swiper.clickedIndex)
-})
+  sliderThumbs.on('click', (swiper) => {
+    swiper.slideTo(swiper.clickedIndex)
+  });
 
-const sliderMain = new Swiper('.slider-main', {
-  loop: true,
-  spaceBetween: 10,
-  loopedSlides: 4,
-});
+  const sliderMain = new Swiper('.slider-main', {
+    loop: true,
+    spaceBetween: 10,
+    loopedSlides: 4,
+  });
 
-sliderThumbs.controller.control = sliderMain;
-sliderMain.controller.control = sliderThumbs;
+  sliderThumbs.controller.control = sliderMain;
+  sliderMain.controller.control = sliderThumbs;
+
+  const videos = document.querySelectorAll('video');
+
+  sliderMain.on('slideChange', () => {
+    for (let i = 0; i < videos.length; i += 1) {
+      videos[i].pause();
+    }
+  });
+
+  const pagination = document.querySelector('.pagination');
+  const paginationButton = document.querySelector('.pagination__arrow');
+
+  paginationButton.addEventListener('click', () => {
+    pagination.classList.toggle('pagination_active')
+  })
+} catch {
+
+}
